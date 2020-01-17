43e9287fce72f71b3800f45b7add7c10a44af1c7
Oleg <ovchilikhin@gmail.com>
2020-01-17 15:54:22 +0400

    Add Signin to Server

--- AUTS_server/config/routes.rb
+++ AUTS_server/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  post 'refresh', controller: :refresh, action: :create
+  post 'signin', controller: :signin, action: :create
+  delete 'signin', controller: :signin, action: :destroy
   resources :themas
   resources :users
 
